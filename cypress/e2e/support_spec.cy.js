c2f92c3d577c4b3a1d61120c77c5b724308cea04
Indumathi Kumar <indu.mtsu@gmail.com>
2023-03-07 12:09:45 +0000

    Updated SpeellMistake andTest Name

--- cypress/e2e/support_spec.cy.js
+++ cypress/e2e/support_spec.cy.js
@@ -1,4 +1,4 @@
-describe("Scenario -1 Test to visit lambatest Selenium  play ground URL & select Deefault value", () => {
+describe("Scenario -1 Test to visit lambatest Selenium  play ground URL & select Default value", () => {
   it("visit the URL", () => {
     cy.visit("https://www.lambdatest.com/selenium-playaround");
     cy.wait(100);
@@ -11,43 +11,45 @@ describe("Scenario -1 Test to visit lambatest Selenium  play ground URL & select
   });
 });
 
-describe("Scenario -1 Test to visit lambatest Selenium  play ground URL & select Deefault value", () => {
-  cy.visit(
-    "https://www.lambdatest.com/selenium-playground/input-form-demo.html"
-  );
-  cy.get('input[name="name"]').type("TestLambda");
-  cy.get('input[name="password"]').type("1234aaaa12");
-  cy.get('textarea[name="address"]').type("123 Main Street");
-  cy.get('input[name="email"]').type("TestLambda.Program@example.com");
-  cy.get('input[name="company"]').type("TestLamda");
-  cy.get('input[name="website"]').type("https://www.lambdatest.com");
-  cy.get('select[name="Country*"]').select("Albania");
-  cy.get('input[name="city"]').type("Albania");
-  cy.get('input[name="address_line2"]').type("123 Main Street");
-  cy.get('input[name="address_line1"]').type("1/250");
-  cy.get('input[id="inputState"]').type("Albania");
-  cy.get('input[name="zip"]').type("10000");
-  cy.get('button[type="submit"]').click();
+describe("Scenario -2 Test to fill the Input form and capture performacce", () => {
+  it("visit the URL", () => {
+    cy.visit(
+      "https://www.lambdatest.com/selenium-playground/input-form-demo.html"
+    );
+    cy.get('input[name="name"]').type("TestLambda");
+    cy.get('input[name="password"]').type("1234aaaa12");
+    cy.get('textarea[name="address"]').type("123 Main Street");
+    cy.get('input[name="email"]').type("TestLambda.Program@example.com");
+    cy.get('input[name="company"]').type("TestLamda");
+    cy.get('input[name="website"]').type("https://www.lambdatest.com");
+    cy.get('select[name="Country*"]').select("Albania");
+    cy.get('input[name="city"]').type("Albania");
+    cy.get('input[name="address_line2"]').type("123 Main Street");
+    cy.get('input[name="address_line1"]').type("1/250");
+    cy.get('input[id="inputState"]').type("Albania");
+    cy.get('input[name="zip"]').type("10000");
+    cy.get('button[type="submit"]').click();
 
-  // Assert that the form gets submitted
-  cy.url().should("include", "input-form-thank-you-demo.html");
+    // Assert that the form gets submitted
+    cy.url().should("include", "input-form-thank-you-demo.html");
 
-  // Verify performance metrics using Cypress Lighthouse commands
-  cy.lighthouse({
-    performance: 50,
-    accessibility: 80,
-    seo: 90,
-    bestPractices: 70,
-    pwa: 50,
-  });
+    // Verify performance metrics using Cypress Lighthouse commands
+    cy.lighthouse({
+      performance: 50,
+      accessibility: 80,
+      seo: 90,
+      bestPractices: 70,
+      pwa: 50,
+    });
 
-  // Validate the success message
-  cy.contains(
-    "Thanks for contacting us, we will get back to you shortly."
-  ).should("be.visible");
+    // Validate the success message
+    cy.contains(
+      "Thanks for contacting us, we will get back to you shortly."
+    ).should("be.visible");
 
-  // Close the browser tab and session
-  cy.window().then((win) => {
-    win.close();
+    // Close the browser tab and session
+    cy.window().then((win) => {
+      win.close();
+    });
   });
 });
